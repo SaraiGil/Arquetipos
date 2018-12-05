c6cdc23e54f7adb1cea4512db5219398e287f080
Sarai <sarai.gil.ramos12@gmail.com>
2018-12-05 09:11:24 -0600

    Last

--- js/main.js
+++ js/main.js
@@ -99,7 +99,7 @@
 
     //owl carousel
     $('.owl-carousel').owlCarousel({
-      autoPlay: 5000, //Set AutoPlay to 3 seconds
+      autoPlay: 8000, //Set AutoPlay to 8 seconds
 
       items : 1,
       itemsDesktop : [1199,1],
